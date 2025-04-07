31660f24f58c692f54661f70edf8561aab1c57c3
Ashish Chaubey <138882318+Ashishchaubey550@users.noreply.github.com>
2025-04-07 17:28:16 +0530

    herosection

--- front-end/src/Components/HeroSection.js
+++ front-end/src/Components/HeroSection.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import ProductList from './ProductList';
+import { CustomNextArrow, CustomPrevArrow } from './SlidderButton';
 
-// Import your images
+// Import images
 import image1 from '../images/image1.webp';
 import image2 from '../images/image2.webp';
 import image3 from '../images/image3.webp';
@@ -11,45 +13,50 @@ import image4 from '../images/image4.webp';
 import image5 from '../images/image5.webp';
 import image6 from '../images/image6.webp';
 import image7 from '../images/image7.webp';
-import ProductList from './ProductList';
-import { CustomNextArrow, CustomPrevArrow } from './SlidderButton';
-function HeroSection() {
-  // Slider settings
-  const settings = {
 
+function HeroSection() {
+  const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
-    prevArrow: <CustomPrevArrow/>, 
-    nextArrow: <CustomNextArrow/>
+    autoplaySpeed: 5000, // More reasonable auto-play duration
+    pauseOnHover: true,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+    adaptiveHeight: true // Better for different image aspect ratios
   };
 
-  const images = [image1, image2, image3 , image4 , image5 , image6 , image7];
+  const images = [image1, image2, image3, image4, image5, image6, image7];
 
   return (
-    <>
-    <div className=' overflow-w--hidden'>
-    <div className="relative h-screen w-full">
-      <Slider {...settings} className="h-full">
-        {images.map((img, index) => (
-          <div key={index}>
-            <img src={img} alt={`Slide ${index + 1}`} className="w-full h-full object-cover" />
-          </div>
-        ))}
-      </Slider>
-    </div>
-    <h1 className=' w-full mt-20 text-7xl font-bold flex justify-center'> NEW ARRIVALS </h1>
-    <div className=' mt-0 flex items-center bg-cover h-screen w-[100%]'>
-        <ProductList/>
+    <div className="overflow-hidden">
+      {/* Hero Carousel */}
+      <div className="relative h-[80vh] w-full"> {/* Better height control */}
+        <Slider {...settings}>
+          {images.map((img, index) => (
+            <div key={index} className="h-[80vh]"> {/* Fixed slide height */}
+              <img 
+                src={img} 
+                alt={`New Arrival ${index + 1}`} // More descriptive alt text
+                className="w-full h-full object-cover object-center" // Better image positioning
+                loading="lazy" // Add lazy loading
+              />
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+      {/* New Arrivals Section */}
+      <div className="container mx-auto px-4 py-12">
+        <h1 className="text-4xl md:text-6xl xl:text-7xl font-bold text-center mb-8">
+          NEW ARRIVALS
+        </h1>
+        <ProductList />
       </div>
     </div>
-
-    </>
-
   );
 }
 
